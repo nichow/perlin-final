ff611a2301fe2a6f407a646b121aa3e32dcd54c8
wxfnyu <wxf201@nyu.edu>
2018-05-11 15:13:45 -0400

    hmm:

---
+++ Assets/Scripts/AsteroidCreation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCreation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public Rigidbody asteroid;
+    public float spawningRadius = 1000;
+    void CreateAsteroid()
+    {
+        Rigidbody asteroidClone = (Rigidbody)Instantiate(asteroid, Random.insideUnitSphere * spawningRadius, Random.rotation);
+    }
+}
+
