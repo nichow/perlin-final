361bd421b1b9d39a2f720e210bd34cf58ee08064
William Fann <wxf201@nyu.edu>
2018-05-12 15:26:21 -0400

    asteroids updated slightly

--- Assets/Scripts/AsteroidCreation.cs
+++ Assets/Scripts/AsteroidCreation.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class AsteroidCreation : MonoBehaviour
 {
+    public static List<Rigidbody> asteroids;
     // Update is called once per frame
+    
     void Update()
     {
         
     }
+
+    void FixedUpdate()
+    {
+           
+    }
+
     public Rigidbody asteroid;
     public float spawningRadius = 1000;
-    void CreateAsteroid()
+    void CreateAsteroid(Vector3 centerSpawningPoint)
     {
-        Rigidbody asteroidClone = (Rigidbody)Instantiate(asteroid, Random.insideUnitSphere * spawningRadius, Random.rotation);
+        if(asteroids == null)
+        {
+            asteroids = new List<Rigidbody>();
+        }
+        Rigidbody asteroidClone = (Rigidbody)Instantiate(asteroid, (Random.insideUnitSphere * spawningRadius) + centerSpawningPoint, Random.rotation);
+        asteroidClone.GetComponent<GravObject>().enabled = true;
+        asteroids.Add(asteroidClone);
+    }
+
+    void removeFarAsteroids(Vector3 location)
+    {
+        foreach (Rigidbody r in asteroids)
+        {
+            if((r.position - location).magnitude > spawningRadius)
+            {
+                asteroids.Remove(r);
+                Destroy(r, 1);
+            }
+        }
     }
 }
 
