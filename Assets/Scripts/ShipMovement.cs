c6e6e47585b0d427709832afc08694e7f2ad92a1
nichow <nhgarcia27@gmail.com>
2018-05-14 20:05:03 -0400

    fix being able to move outside the vp of the camera

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -6,6 +6,8 @@ public class ShipMovement : MonoBehaviour
 {
     private float _thrust;
     private Rigidbody _rb;
+    private Renderer _rend;
+    public Camera Camera;
 
     // Use this for initialization
     void Start ()
@@ -19,21 +21,23 @@ public class ShipMovement : MonoBehaviour
     {
         var hdirection = Input.GetAxis("Horizontal"); // negative if left, positive if right
         var vdirection = Input.GetAxis("Vertical");   // negative if down, positive if up
+        //position of object relative to the viewport (0, 0) is bottom left; (pixelWidth, pixelHeight) is top right
+        var screenPos = Camera.WorldToScreenPoint(transform.position);
 
-        if (hdirection < 0)
+        if (hdirection < 0 && screenPos.x > 0)
         {
             transform.Translate(.2f * Vector3.left);
         }
-        else if (hdirection > 0)
+        else if (hdirection > 0 && screenPos.x < Camera.pixelWidth)
         {
             transform.Translate(.2f * Vector3.right);
         }
 
-        if (vdirection > 0)
+        if (vdirection > 0 && screenPos.y < Camera.pixelHeight)
         {
             transform.Translate(.2f * Vector3.up);
         }
-        else if (vdirection < 0)
+        else if (vdirection < 0 && screenPos.y > 0)
         {
             transform.Translate(.2f * Vector3.down);
         }
@@ -44,4 +48,9 @@ public class ShipMovement : MonoBehaviour
 	{
         _rb.AddForce(Vector3.forward * _thrust * Time.fixedDeltaTime);
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
 }
--- Assets/Scripts/ShipShoot.cs
+++ Assets/Scripts/ShipShoot.cs
@@ -14,14 +14,19 @@ public class ShipShoot : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
 	{
+        //create layerMask just containing the player object
 	    var layerMask = 1 << 8;
-
+        //invert the mask so the player collider is ignored for purposes of ray creation
         layerMask = ~layerMask;
 		if (Input.GetButtonDown("Fire"))
 		{
 		    RaycastHit hit;
 		    if (Physics.Raycast(transform.position, Vector3.forward, out hit, 15f, layerMask))
 		    {
+		        if (hit.collider.gameObject.tag == "Asteroid")
+		        {
+                    Destroy(hit.collider.gameObject);
+		        }
 		        Debug.Log("hit");
 		    }
 		    else
