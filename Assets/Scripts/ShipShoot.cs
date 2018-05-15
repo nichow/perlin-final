8374edeca5a1baa498d625423d87b3b60ae99c86
nichow <nhgarcia27@gmail.com>
2018-05-15 03:51:15 -0400

    add line renderer functionality

--- Assets/Scripts/ShipShoot.cs
+++ Assets/Scripts/ShipShoot.cs
@@ -4,11 +4,16 @@ using System.Diagnostics;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
-public class ShipShoot : MonoBehaviour {
+public class ShipShoot : MonoBehaviour
+{
+
+    private LineRenderer _path;
+    private Vector3[] _points;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    _path = GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -19,11 +24,14 @@ public class ShipShoot : MonoBehaviour {
         //invert the mask so the player collider is ignored for purposes of ray creation
         layerMask = ~layerMask;
 		if (Input.GetButtonDown("Fire"))
-		{
-		    var forward = transform.TransformDirection(Vector3.forward) * 15;
-		    Debug.DrawRay(transform.position, forward, Color.green, 15, true);
+        {   
+		    var forward = transform.TransformDirection(Vector3.forward) * 50;
+            var pathInit = Vector3.zero;
+            _path.SetPosition(0, pathInit);
+//		    Debug.DrawRay(transform.position, forward, Color.green, 15, true);
+		    StartCoroutine(ShotEffect());
             RaycastHit hit;
-		    if (Physics.Raycast(transform.position, forward, out hit, 15f, layerMask))
+		    if (Physics.Raycast(transform.position, forward, out hit, 50f, layerMask))
 		    {
 		        if (hit.collider.gameObject.tag == "Asteroid")
 		        {
@@ -35,6 +43,14 @@ public class ShipShoot : MonoBehaviour {
 		    {
                 Debug.Log("No Hit");
 		    }
-		}
+            _path.SetPosition(1, Vector3.zero + (Vector3.forward * 50));
+        }
 	}
+
+    private IEnumerator ShotEffect()
+    {
+        _path.enabled = true;
+        yield return new WaitForSeconds(.5f);
+        _path.enabled = false;
+    }
 }
